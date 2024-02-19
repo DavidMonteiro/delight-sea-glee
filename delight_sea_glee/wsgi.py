99b306b94f15b324560a66412526c2aad264831f
David Monteiro <david.frtd.monteiro@gmail.com>
2024-02-19 06:26:17 +0000

    Created project delight_sea_glee and added app reservation_system

---
+++ delight_sea_glee/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for delight_sea_glee project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'delight_sea_glee.settings')
+
+application = get_wsgi_application()
