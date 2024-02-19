61f14adadc3052dbb7787e64a58628730d98a4c8
David Monteiro <david.frtd.monteiro@gmail.com>
2024-02-19 06:39:46 +0000

    Heroku preparation for deployment

--- delight_sea_glee/settings.py
+++ delight_sea_glee/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5g)kg=xl@_p(-6t#ro9vpgs9bw0hh2%*7)+a0pibfd3g%yw*-_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-davidmontei-delightseag-hyx96pbf0f2.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-davidmontei-delightseag-hyx96pbf0f2.ws-eu108.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
